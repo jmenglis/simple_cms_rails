81d32c4b65eceeffbb230b879eb3d6e0d795fdad
Josh English <josh@joshenglish.com>
2016-06-15 10:32:56 -0500

    Making changes to the migrate file for User

--- db/migrate/20160615152826_create_users.rb
+++ db/migrate/20160615152826_create_users.rb
@@ -1,8 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
-
-      t.timestamps null: false
+      t.column "first_name", :string, :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => "", :null => false
+      t.string "password", :limit => 40
+      # The t.timestamps handles this already
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+      t.timestamps
     end
   end
+
+  def down
+    drop_table :users
+  end
+
 end
