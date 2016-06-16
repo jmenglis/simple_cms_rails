d2bf0c1ba914dcc9709459c4ecf5f45d49834374
Josh English <josh@joshenglish.com>
2016-06-16 10:57:43 -0500

    Added the completed Subjects CRUD

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -7,14 +7,63 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new(:name => "Default")
   end
+
+  def create
+    # Instantiate new object using params
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # If save is done, redirect to index
+      flash[:notice] = "Subject created successfully"
+      redirect_to(:action => 'index')
+    else
+      # If save fails, redisplay form with params
+      render('new')
+    end
+  end
+
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
-  def delete
+  def update
+    # Find existing object using params
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If update is done, redirect to index
+      flash[:notice] = "Subject updated successfully"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      # If update fails, redisplay form with params
+      render('edit')
+    end
   end
+
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' destroyed successfully"
+    redirect_to(:action => 'index')
+  end
+
+
+  # Private method for the params.
+  private
+    # same as params[:subject], except the only data that can be mass assigned is listed.
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
 end
