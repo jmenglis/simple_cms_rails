f68efd85f2895f669085a6ed2b03083ccf8bfae1
Josh English <josh@joshenglish.com>
2016-06-23 15:32:41 -0500

    Added login form and adjusted controller to confirm information stored in DB

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,31 @@
 class AccessController < ApplicationController
   def index
+    # display text
   end
 
   def login
+    # display login form
   end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(:username => params[:username]).first
+      if founder_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+    if authorized_user
+      flash[:notice] = "You are now logged in."
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Invalid username/password combination."
+      redirect_to(:action => 'login')
+    end
+  end
+
+  def logout
+    flash[:notice] = "Logged Out"
+    redirect_to(:action => "login")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'access/index'
-
-  get 'access/login'
-
   # get 'demo/index'
   root "demo#index"
+
+  get 'admin', :to => 'access#index'
 
   match ':controller(/:action)(/:id)', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
