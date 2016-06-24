9dad8bb4707fbae53acf76b2c8cd3388c26ad0e7
Josh English <josh@joshenglish.com>
2016-06-24 12:26:27 -0500

    Adding public.html.erb and layout to Public Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'public/index'
 
-  get 'public/show'
 
   # get 'demo/index'
-  root "demo#index"
+  root "public#index"
 
+  get 'show/:permalink', :to => 'public#show'
   get 'admin', :to => 'access#index'
 
   match ':controller(/:action)(/:id)', :via => [:get, :post]
